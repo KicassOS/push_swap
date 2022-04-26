06d378053e79a2f0790ba3c8bbb2e9959eff487b
Pierre SZLEPER <pszleper@e3r5p2.42.fr>
2022-04-26 15:48:30 +0200

    Added libft and ft_printf

---
+++ Libft/ft_strncmp.c
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strncmp.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: pszleper <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/12/03 09:55:46 by pszleper          #+#    #+#             */
+/*   Updated: 2021/12/06 23:19:52 by pszleper         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+int	ft_strncmp(const char *s1, const char *s2, size_t n)
+{
+	size_t	i;
+	int		result;
+
+	i = 0;
+	if (n == 0)
+	{
+		return (0);
+	}
+	while (s1[i] && s2[i] && s1[i] == s2[i] && i + 1 < n)
+	{
+		i++;
+	}
+	result = (unsigned char) s1[i] - (unsigned char) s2[i];
+	if (result > 0)
+		return (1);
+	if (result < 0)
+		return (-1);
+	return (result);
+}
