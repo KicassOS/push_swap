04e4e4003785a96ba9fd4dae01beae1d92de7a38
Pierre SZLEPER <psz@ubuntu.com>
2022-12-15 06:18:23 +0100

    Norminette for both stack_utils Renamed stack_utils.c to stack_utils_1.c

---
+++ stack_utils_2.c
@@ -0,0 +1,47 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   stack_utils_2.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: pszleper <pszleper@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/12/15 06:10:20 by pszleper          #+#    #+#             */
+/*   Updated: 2022/12/15 06:11:23 by pszleper         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "push_swap.h"
+
+/*
+  This function returns the smallest number in a stack
+*/
+int	ft_min(t_stack *a)
+{
+	int		i;
+
+	i = a->nbr;
+	while (a)
+	{
+		if (a->nbr < i)
+			i = a->nbr;
+		a = a->next;
+	}
+	return (i);
+}
+
+/*
+  This function returns the biggest number in a stack
+*/
+int	ft_max(t_stack *a)
+{
+	int		i;
+
+	i = a->nbr;
+	while (a)
+	{
+		if (a->nbr > i)
+			i = a->nbr;
+		a = a->next;
+	}
+	return (i);
+}
