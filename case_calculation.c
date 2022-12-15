1685e626b82cea73fa3e2ed3c64322f6497e021f
Pierre SZLEPER <psz@ubuntu.com>
2022-12-15 06:09:24 +0100

    case_calculation norminette

---
+++ case_calculation.c
@@ -0,0 +1,61 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   case_calculation.c                                 :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: pszleper <pszleper@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/12/15 06:01:01 by pszleper          #+#    #+#             */
+/*   Updated: 2022/12/15 06:08:54 by pszleper         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "push_swap.h"
+
+/*
+  The following functions calculate the cost of each combination of rotations
+  for all 4 possible cases, rarb, rrarb, rarrb, rrarrb
+*/
+int	ft_case_rarb(t_stack *a, t_stack *b, int c)
+{
+	int	i;
+
+	i = ft_find_place_b(b, c);
+	if (i < ft_find_index(a, c))
+		i = ft_find_index(a, c);
+	return (i);
+}
+
+int	ft_case_rrarb(t_stack *a, t_stack *b, int c)
+{
+	int	i;
+
+	i = 0;
+	if (ft_find_index(a, c))
+		i = ft_lstsize(a) - ft_find_index(a, c);
+	i = ft_find_place_b(b, c) + i;
+	return (i);
+}
+
+int	ft_case_rarrb(t_stack *a, t_stack *b, int c)
+{
+	int	i;
+
+	i = 0;
+	if (ft_find_place_b(b, c))
+		i = ft_lstsize(b) - ft_find_place_b(b, c);
+	i = ft_find_index(a, c) + i;
+	return (i);
+}
+
+int	ft_case_rrarrb(t_stack *a, t_stack *b, int c)
+{
+	int	i;
+
+	i = 0;
+	if (ft_find_place_b(b, c))
+		i = ft_lstsize(b) - ft_find_place_b(b, c);
+	if ((i < (ft_lstsize(a) - ft_find_index(a, c))) && ft_find_index(a, c))
+		i = ft_lstsize(a) - ft_find_index(a, c);
+	return (i);
+}
